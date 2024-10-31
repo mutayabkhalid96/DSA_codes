0f52de3a9b334991f4d3abbb5f198c9f51c26106
Mutayab Khalid <mutayabkhalid96@gmail.com>
2024-10-31 19:57:06 +0100

    Binary search example

---
+++ algorithms/binary_search_example.py
@@ -0,0 +1,20 @@
+def search_indices(number_list, findnumber):
+  index = binary_recursive(number_list, findnumber)
+  indices = [index]
+  i = index - 1
+  while i >= 0:
+    if findnumber == number_list[i]:
+      indices.append(i)
+    else:
+      break
+    i = i - 1
+
+  i = index + 1
+  while i < len(number_list):
+    if findnumber == number_list[i]:
+      indices.append(i)
+    else:
+      break
+    i = i + 1
+
+  return sorted(indices)
